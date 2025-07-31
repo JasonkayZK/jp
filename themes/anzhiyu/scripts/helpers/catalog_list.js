f5b43b66c5e4cc3506a43ba56112558950f756f3
jasonkayzk <jasonkayzk@gmail.com>
2025-07-31 15:54:58 +0800

    fix: url

--- themes/anzhiyu/scripts/helpers/catalog_list.js
+++ themes/anzhiyu/scripts/helpers/catalog_list.js
@@ -3,7 +3,7 @@ hexo.extend.helper.register("catalog_list", function (type) {
   hexo.locals.get(type).map(function (item) {
     html += `
     <div class="catalog-list-item" id="/${item.path}">
-      <a href="/${item.path}">
+      <a href="/jp/${item.path}">
         ${item.name}
       </a>
     </div>
